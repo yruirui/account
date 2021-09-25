bf60d314472f577ed1261a06edf7462fd29be647
ruirui <yruirui96@gmail.com>
2021-09-25 15:55:53 +0800

    使用ts的componet的porp报错；

--- src/components/Types.vue
+++ src/components/Types.vue
@@ -1,52 +1,73 @@
 <template>
   <ul class='types'>
-    <li :class="type==='-'&& 'selected'" @click="selectType('-')" >收入</li>
+    <li :class="type==='-'&& 'selected'" @click="selectType('-')">收入</li>
     <li :class="type==='+'&& 'selected'" @click="selectType('+')">支出</li>
   </ul>
 </template>
 
-<script>
-export default {
-  name: "Types",
-  data(){
-    return{
-      type:'-',
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component({
+  props: {
+    propMessage: String,
+  }
+})
+export default class Types extends Vue {
+  type = '-';
+  helloMsg='hello'+this.propMessage;
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
-  },
-  methods:{
-    selectType(type){
-      if(type!=='-' && type!=='+'){
-        throw new Error('type is unknown')
-      }
-      this.type=type
-    }
+    this.type = type;
   }
 }
+// export default {
+//   // name: "Types",
+//   // data(){
+//   //   return{
+//   //     type:'-',
+//   //   }
+//   // },
+//   // methods:{
+//   //   selectType(type){
+//   //     if(type!=='-' && type!=='+'){
+//   //       throw new Error('type is unknown')
+//   //     }
+//   //     this.type=type
+//   //   }
+//   // }
+// }
 
 </script>
 
 <style scoped lang='scss'>
 @import '~@/assets/style/helper.scss';
-.types{
-  background:#c4c4c4;
+
+.types {
+  background: #c4c4c4;
   display: flex;
-  text-align:center;
-  font-size:24px;
-  >li{
-    width:50%;
-    height:64px;
-    display:flex;
-    justify-content:center;
+  text-align: center;
+  font-size: 24px;
+
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
     align-items: center;
     position: relative;
-    &.selected::after{
-      content:'';
-      position:absolute;
-      bottom:0;
-      left:0;
+
+    &.selected::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
       width: 100%;
       height: 4px;
-      background:#333;
+      background: #333;
 
     }
   }
